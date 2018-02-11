e101952d76ef82d48287ff1fca22db9a680f02f4
Adam Pedley <adam.pedley@gmail.com>
2018-02-11 19:00:19 +1100

    Corrected Android issue with LineHeights

--- Xamarin.Forms.Platform.Android/GestureManager.cs
+++ Xamarin.Forms.Platform.Android/GestureManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using Android.Support.V4.View;
@@ -91,7 +92,7 @@ namespace Xamarin.Forms.Platform.Android
 				if (Element is Label label)
 					return label.FormattedText?.Spans;
 
-				return null;
+				return new List<Span>();
 			}),
 				new PanGestureHandler(() => View, context.FromPixels));
 
